dfc4f94d48fa7f85e3caacad10ccbbe43420498f
ZoraZora <zora759gk@gmail.com>
2020-07-20 13:07:37 +0800

    添加测试类，补充注释
    引用hutool-extra的随机工具包

--- mc-sdk/src/main/java/cn/gk/multilevel/cache/sdk/model/CacheConfiguration.java
+++ mc-sdk/src/main/java/cn/gk/multilevel/cache/sdk/model/CacheConfiguration.java
@@ -17,11 +17,26 @@ import java.io.Serializable;
 @Builder
 public class CacheConfiguration implements Serializable {
 
+    /**
+     * 默认配置内存用量 128*1024*1024 Bytes = 128 MB（以权重配置为限制的话）
+     */
     private static final long DEFAULT_MAXIMUM_MEMORY_USAGE_SIZE = 128 * 1024 * 1024;
-    private static final long DEFAULT_MAXIMUM_CACHE_SIZE = 256;
+    /**
+     * 默认权重公式：Key的Bytes+Value的Bytes
+     */
+    private static final Weigher<String, String> DEFAULT_WEIGHER = (key, value) -> key.getBytes().length + value.getBytes().length;
+    /**
+     * 默认最大本地缓存个数（以缓存个数为限制的话）
+     */
+    private static final long DEFAULT_LOCAL_MEMORY_MAXIMUM_CACHE_KEY_SIZE = 256;
+    /**
+     * 默认Redis缓存失效时间：3*60 秒 = 3 分钟
+     */
     private static final long DEFAULT_REDIS_TTL = 3 * 60;
-    private static final int DEFAULT_KEY_COUNT_IN_SINGLE_WINDOW = 128;
-    private static final Weigher<String, String> DEFAULT_WEIGHER = (key, value) -> key.length() + value.length();
+    /**
+     * 默认单窗口统计的Key个数
+     */
+    private static final int DEFAULT_KEY_COUNT_IN_SINGLE_WINDOW = 256;
 
     /**
      * 获取权重计算公式
@@ -65,8 +80,9 @@ public class CacheConfiguration implements Serializable {
      * @return 内存缓存个数
      */
     public long getLocalCacheCountSize() {
-        return localCacheCountSize > 0 ? localCacheCountSize : DEFAULT_MAXIMUM_CACHE_SIZE;
+        return localCacheCountSize > 0 ? localCacheCountSize : DEFAULT_LOCAL_MEMORY_MAXIMUM_CACHE_KEY_SIZE;
     }
+
     /**
      * 获取单个时间窗内统计的最大key个数
      *
@@ -90,15 +106,20 @@ public class CacheConfiguration implements Serializable {
     private final long redisTtl;
     /**
      * 本地缓存最大个数配置
+     * 该参数即保存在本地缓存中的返回结果个数，当返回结果较长时，该参数会显著影响缓存的内存占用率。
+     * 建议配置在服务内存足够承载的范围内，或不要对该值进行修改，去配置自定义权重计算公式和权重最大值，可以获得更大的自由度。
      */
     private final long localCacheCountSize;
     /**
      * 单个时间窗内统计的最大key个数
+     * 该参数对内存占用的影响相比本地缓存最大个数（localCacheCountSize）要低，因此建议适当调高以获得更高的内存缓存命中效率
+     * 建议将该值设置大于服务中使用MC的方法个数，在不超过预期总返回结果数的情况下，提高该值可以获得更高的内存命中率.
      */
     private final int singleWindowMaximumKeyCount;
 
     /**
      * value软引用
+     * 配置内存缓存中的数据是否可以以软引用的方式存在，降低内存顶满的风险
      */
     @Getter
     private final boolean softValues;
--- mc-test/src/test/java/cn/gk/multilevel/cache/test/CacheSdkTest.java
+++ mc-test/src/test/java/cn/gk/multilevel/cache/test/CacheSdkTest.java
@@ -1,6 +1,7 @@
 package cn.gk.multilevel.cache.test;
 
 import cn.gk.multilevel.cache.sdk.api.McTemplate;
+import cn.hutool.core.util.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +9,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootTest
 @Slf4j
@@ -17,51 +21,78 @@ public class CacheSdkTest {
     @Autowired
     private McTemplate multilevelCacheTemplate;
 
+    class ActAsNormalUser implements Runnable {
+
+        CountDownLatch countDownLatch;
+        String keyPrefix;
+        int counter;
+
+        ActAsNormalUser(CountDownLatch countDownLatch, String keyPrefix, int counter) {
+            this.countDownLatch = countDownLatch;
+            this.keyPrefix = keyPrefix;
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            try {
+                countDownLatch.await(); // 等待其它线程
+                for (int i = 0; i < 99999; i++) {
+                    String key = keyPrefix + (RandomUtil.randomInt(1,counter));
+                    log.info("激活-{}", key);
+                    multilevelCacheTemplate.tryGetValue(key, String.class);
+                    try {
+                        Thread.sleep(50);
+                    }catch (Exception ignore){}
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Test
     public void redisWriteAndGet() {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        int low = 99;
+        int high = 5;
         String keyPrefix = "测试key";
         String valuePrefix = "测试";
-        for (int i=0;i<7;i++){
-            String key = keyPrefix+(i+1);
-            log.info("激活-{}",key);
-            multilevelCacheTemplate.putObjectIntoCache(key, valuePrefix+(i+1));
+        for (int i = 0; i < low; i++) {
+            String key = keyPrefix + (i + 1);
+            log.info("激活-{}", key);
+            multilevelCacheTemplate.putObjectIntoCache(key, valuePrefix + (i + 1));
         }
-        for (int i=0;i<7;i++){
-            String key = keyPrefix+(i+1);
-            log.info("激活-{}",key);
-            multilevelCacheTemplate.tryGetValue(key, String.class);
+        new Thread(new ActAsNormalUser(countDownLatch,keyPrefix,low)).start();
+        countDownLatch.countDown();
+        new Thread(new ActAsNormalUser(countDownLatch,keyPrefix,high)).start();
+        countDownLatch.countDown();
+        try {
+            Thread.sleep(1000 * 5);
+        } catch (Exception ignore) {
         }
-        for (int i=0;i<3;i++){
-            String key = keyPrefix+(i+1);
-            log.info("激活-{}",key);
+        for (int i = 0; i < low; i++) {
+            String key = keyPrefix + (i + 1);
+            log.info("激活-{}", key);
+            multilevelCacheTemplate.putObjectIntoCache(key, valuePrefix + (i + 1));
+        }
+        for (int i = 0; i < low; i++) {
+            String key = keyPrefix + (i + 1);
+            log.info("激活-{}", key);
             multilevelCacheTemplate.tryGetValue(key, String.class);
         }
         try {
-            Thread.sleep(1000*5);
-        }catch (Exception ignore){
+            Thread.sleep(1000 * 5);
+        } catch (Exception ignore) {
         }
-        for (int i=0;i<7;i++){
-            String key = keyPrefix+(i+1);
-            log.info("激活-{}",key);
-            multilevelCacheTemplate.putObjectIntoCache(key, valuePrefix+(i+1));
-        }
-        for (int i=0;i<7;i++){
-            String key = keyPrefix+(i+1);
-            log.info("激活-{}",key);
+        for (int i = 0; i < low; i++) {
+            String key = keyPrefix + (i + 1);
+            log.info("激活-{}", key);
             multilevelCacheTemplate.tryGetValue(key, String.class);
         }
-        try {
-            Thread.sleep(1000*5);
-        }catch (Exception ignore){
-        }
-        for (int i=0;i<7;i++){
-            String key = keyPrefix+(i+1);
-            log.info("激活-{}",key);
-            multilevelCacheTemplate.tryGetValue(key, String.class);
-        }
-        Assert.assertNull(multilevelCacheTemplate.tryGetValue(keyPrefix+"6",String.class));
+        Assert.assertNull(multilevelCacheTemplate.tryGetValue(keyPrefix + (low + 1), String.class));
         log.info("通过LRU检验");
-        Assert.assertEquals(multilevelCacheTemplate.tryGetValue(keyPrefix+"1",String.class),valuePrefix+"1");
+        Assert.assertEquals(multilevelCacheTemplate.tryGetValue(keyPrefix + 1, String.class), valuePrefix + 1);
         log.info("通过Get检验");
     }
 }
